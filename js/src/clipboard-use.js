276ec488e8bd28d721a2eea7164c6069f1e0779e
mango185 <mango185@163.com>
2020-06-10 22:17:57 +0800

    Site updated: 2020-06-10 22:17:40

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Jun 04 2020 07:18:55 GMT+0800 (GMT+08:00)
+// build time:Wed Jun 10 2020 22:17:26 GMT+0800 (GMT+08:00)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
