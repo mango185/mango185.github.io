dfbea501f07ae1494b63a9d35d39d568988581a9
mango185 <mango185@163.com>
2023-08-21 19:27:16 +0800

    Site updated: 2023-08-21 19:27:08

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 20 2023 20:27:04 GMT+0800 (中国标准时间)
+// build time:Mon Aug 21 2023 19:26:56 GMT+0800 (中国标准时间)
 !function(n,t,a){var i=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>复制</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};i()}(window,document);
 //rebuild by neat 
